97d00f2f4d0c54c186417b184fbff18dc6bced90
Cristina <cristinarodriguez.palmer@gmail.com>
2017-02-15 17:38:22 +0100

    Punto 2 práctica 9 acabado

--- Practica9/aciertaNumero.js
+++ Practica9/aciertaNumero.js
@@ -5,19 +5,29 @@ $(document).ready(function(){
 });
 // 2.2. Enviar petición ajax a aciertaNumeroXML.php con parámetro inicio=si
 function numeroAleatorio(){
-    $.ajax({
-        type:"GET",
-        url:"aciertaNumeroXML.php",
-        dataType:"json",
-        data:{"inicio": "si"},
-        success: function(respuesta){
-            console.log(respuesta);
-            /*var aleatorio = respuesta.resp;
-            console.log(respuesta.resp);
-            ("#mensaje").html("Se ha generado un nuevo número");*/
-        },
-        error : function(xhr, status) {
-            alert('Ha habido un error -> '+xhr+", "+status);
+    var xmlHttp = new XMLHttpRequest();
+    
+    // url destino + parámetros
+    xmlHttp.open("GET", "aciertaNumeroXML.php?inicio=si", true);
+    xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    // qué hacer con la respuesta, quién recibe
+    xmlHttp.onreadystatechange = function(){
+        // state 4 = completado
+        if(xmlHttp.readyState === 4){
+            recibirNumero(xmlHttp);
         }
-    });
+    }
+    // enviar la petición + los datos que queramos
+    xmlHttp.send(null);
+}
+
+function recibirNumero(xmlHttp){
+    if(xmlHttp.status === 200){
+        var respuesta = xmlHttp.responseXML;
+
+        var numAleatorio = respuesta.getElementsByTagName("inicio")[0].textContent;
+        console.log(numAleatorio);
+        document.getElementById("mensaje").innerHTML = "Se ha generado un nuevo número!!";
+    }
 }

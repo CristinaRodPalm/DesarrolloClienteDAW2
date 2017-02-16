762f165e6f66691e44d4730428b9a9c40900408a
Cristina <cristinarodriguez.palmer@gmail.com>
2017-02-16 19:01:34 +0100

    Punto 4 práctica 9 acabado

--- Practica9/aciertaNumero.js
+++ Practica9/aciertaNumero.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
    $("#inicioXML").click(numeroAleatorio);
    $("#checkAjaxXML").click(comprobarNumUsuario);
+   $("#inicioJSON").click(numeroAleatorioJSON);
+   $("#checkAjaxJSON").click(comprobarNumUsuarioJSON);
     
 });
-// 2.2. Enviar petición ajax a aciertaNumeroXML.php con parámetro inicio=si
+
 function numeroAleatorio(){
     var xmlHttp = new XMLHttpRequest();
     
@@ -42,9 +44,7 @@ function comprobarNumUsuario(){
     xmlHttp.open("GET", ruta, true);
     xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xmlHttp.onreadystatechange = function(){
-        if(xmlHttp.readyState === 4){
-            verificarNumero(xmlHttp);
-        }
+        if(xmlHttp.readyState === 4) verificarNumero(xmlHttp);
     }
     xmlHttp.send(null);
 }
@@ -54,6 +54,65 @@ function verificarNumero(xmlHttp){
         var respuesta = xmlHttp.responseXML;
         var encontrado = respuesta.getElementsByTagName("encontrado")[0].textContent;
         var mensaje = respuesta.getElementsByTagName("mensaje")[0].textContent;
+        document.getElementById("mensaje").innerHTML = mensaje;
+        if(encontrado === "si"){
+            document.getElementById("encontrado").innerHTML = encontrado;
+        }
+    }
+}
+
+function numeroAleatorioJSON(){
+    var xmlHttp = new XMLHttpRequest();
+    
+    // url destino + parámetros
+    xmlHttp.open("GET", "aciertaNumeroJSON.php?inicio=si", true);
+    xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    // qué hacer con la respuesta, quién recibe
+    xmlHttp.onreadystatechange = function(){
+        // state 4 = completado
+        if(xmlHttp.readyState === 4){
+            recibirNumeroJSON(xmlHttp);
+        }
+    }
+    // enviar la petición + los datos que queramos
+    xmlHttp.send(null);
+}
+
+function recibirNumeroJSON(xmlHttp){
+    if(xmlHttp.status === 200){
+        var respuesta = xmlHttp.responseText;
+        
+        var respuestaJSON = JSON.parse(respuesta);
+        
+        var numero = respuestaJSON.inicio;
+        console.log(numero);
+        document.getElementById("mensaje").innerHTML = "Se ha generado un nuevo número!!";
+    }
+}
+
+function comprobarNumUsuarioJSON(){
+    var numUsuario = document.getElementById("numero").value;
+    
+    // PETICIÓN AJAX
+    var xmlHttp = new XMLHttpRequest();
+    var ruta = "aciertaNumeroJSON.php?numero="+numUsuario;
+    xmlHttp.open("GET", ruta, true);
+    xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlHttp.onreadystatechange = function(){
+        if(xmlHttp.readyState === 4) verificarNumeroJSON(xmlHttp);
+    }
+    xmlHttp.send(null);
+}
+
+function verificarNumeroJSON(xmlHttp){
+    if(xmlHttp.status === 200){
+        var respuesta = xmlHttp.responseText;
+        var respuestaJSON = JSON.parse(respuesta);
+        
+        var encontrado = respuestaJSON.encontrado;
+        var mensaje = respuestaJSON.mensaje;
+      
         document.getElementById("mensaje").innerHTML = mensaje;
         if(encontrado === "si"){
             document.getElementById("encontrado").innerHTML = encontrado;

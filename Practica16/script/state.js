614c9b1846ff99caab0ae015cc480ce38a13c62f
Cristina <cristinarodriguez.palmer@gmail.com>
2017-04-26 00:37:43 +0200

    P16 acabada :D

--- Practica16/script/state.js
+++ Practica16/script/state.js
@@ -7,13 +7,81 @@ app.config(function ($stateProvider, $urlRouterProvider) {
                 views: {
                     'navbar@': {
                             templateUrl: 'navbar/misListas.html'
-                        //controller: 'navbarCtrl'
                     },
                     'content@': {
                         templateUrl: 'vistas/misListas.html'
                     },
                     'footer@': {
-                        templateUrl: 'footer/misListas.html'
+                        templateUrl: 'footer.html'
+                    }
+                }
+            })
+            .state('misListas', {
+                url: '/',
+                views: {
+                    'navbar@': {
+                            templateUrl: 'navbar/misListas.html'
+                    },
+                    'content@': {
+                        templateUrl: 'vistas/misListas.html'
+                    },
+                    'footer@': {
+                        templateUrl: 'footer.html'
+                    }
+                }
+            })
+            .state('suscritas', {
+                url: '/suscritas',
+                views: {
+                    'navbar@': {
+                            templateUrl: 'navbar/suscritas.html'
+                    },
+                    'content@': {
+                        templateUrl: 'vistas/suscritas.html'
+                    },
+                    'footer@': {
+                        templateUrl: 'footer.html'
+                    }
+                }
+            })
+            .state('buscar', {
+                url: '/buscar',
+                views: {
+                    'navbar@': {
+                            templateUrl: 'navbar/buscar.html'
+                    },
+                    'content@': {
+                        templateUrl: 'vistas/buscar.html'
+                    },
+                    'footer@': {
+                        templateUrl: 'footer.html'
+                    }
+                }
+            })
+            .state('listaCompra', {
+                url: '/lista/listaCompra',
+                views: {
+                    'navbar@': {
+                            templateUrl: 'navbar/listaCompra.html'
+                    },
+                    'content@': {
+                        templateUrl: 'vistas/listaCompra.html'
+                    },
+                    'footer@': {
+                        templateUrl: 'footer.html'
+                    }
+                }
+            }).state('misDatos', {
+                url: '/misDatos',
+                views: {
+                    'navbar@': {
+                            templateUrl: 'navbar/misDatos.html'
+                    },
+                    'content@': {
+                        templateUrl: 'vistas/misDatos.html'
+                    },
+                    'footer@': {
+                        templateUrl: 'footer.html'
                     }
                 }
             })
